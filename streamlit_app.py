6c1cb3136051a39a9deca8b2ac81d0b3312ef4a8
Tim Kluge <ubrfu@student.kit.edu>
2024-10-16 23:12:30 +0200

    fix: update formatting

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,5 +2,5 @@ import streamlit as st
 
 st.title("Rate the difficulty of Minecraft Missions!")
 st.write(
-    f"Using database schema {st.secrets["DB_SCHEMA"]}..."
+    f'Using database schema "{st.secrets.DB_SCHEMA}"'
 )
