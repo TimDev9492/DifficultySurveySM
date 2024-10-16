f6e0149d35e9f21214733f3af25f880739232ce3
Tim Kluge <ubrfu@student.kit.edu>
2024-10-16 23:11:39 +0200

    chore: add supabase dependency

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,6 @@
 import streamlit as st
 
-st.title("🎈 Rate the difficulty of Minecraft Missions!")
+st.title("Rate the difficulty of Minecraft Missions!")
 st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
+    f"Using database schema {st.secrets["DB_SCHEMA"]}..."
 )
