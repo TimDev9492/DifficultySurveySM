cf3e6489bfbe48e49ccd3ddf4520f14abb2617d8
Tim Kluge <ubrfu@student.kit.edu>
2024-10-17 18:04:40 +0200

    fix: adjust welcome message

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,5 +6,5 @@ st_supabase_client.client.postgrest.schema(st.secrets.DB_SCHEMA or 'dev')
 
 st.title("Rate the difficulty of Minecraft Missions!")
 st.write(
-    f'Using database schema "{st.secrets.DB_SCHEMA}"'
+    f'Select from the options on the sidebar to rate difficulties!'
 )
