4b71bffd8ea54bd1ff741f9806285c103eccf0ac
Tim Kluge <ubrfu@student.kit.edu>
2024-10-21 19:54:14 +0200

    feat: add button to go back and edit history

--- pages/2_Rate_Mob_Kill_Difficulty.py
+++ pages/2_Rate_Mob_Kill_Difficulty.py
@@ -7,6 +7,11 @@ st_supabase_client.client.postgrest.schema(st.secrets.DB_SCHEMA or 'dev')
 
 session_state_key = 'mob'
 finished_flag_key = f'{session_state_key}_finished'
+history_key = f'{session_state_key}_history'
+
+# setup history
+if not history_key in st.session_state:
+    st.session_state[history_key] = []
 
 def get_database_entry(persist_state=True):
     if persist_state and session_state_key in st.session_state:
@@ -47,7 +52,7 @@ def update_difficulty(difficulty, entry):
     }).eq(
         'id', entry['id']
     ).execute()
-    return len(response.data) > 0
+    return response
 
 get_database_entry()
 
@@ -62,14 +67,17 @@ st.title(st.session_state[session_state_key]['name'])
 st.divider()
 difficulty = st.slider(
     "How difficult is it to kill this mob? (1=Very Easy, 10=Impossible)",
-    1, 10, 5
+    1, 10, 5 if session_state_key not in st.session_state or st.session_state[session_state_key]['difficulty'] == None else st.session_state[session_state_key]['difficulty']
 )
 
-left, right = st.columns(2)
+left, middle, right = st.columns(3)
 
 def save_difficulty_load_new_item():
     msg = st.toast("Saving changes...")
-    success = update_difficulty(difficulty, st.session_state[session_state_key])
+    updated = update_difficulty(difficulty, st.session_state[session_state_key])
+    success = len(updated.data) > 0
+    # push updated item to history
+    st.session_state[history_key].append(updated.data[0])
     if success:
         msg.toast(f'Saved difficulty {difficulty} for "{st.session_state[session_state_key]["name"]}"')
     else:
@@ -80,5 +88,12 @@ def skip_entry():
     st.toast(f'Skipping "{st.session_state[session_state_key]["name"]}"...')
     get_database_entry(persist_state=False)
 
-left.button("Skip", use_container_width=True, on_click=skip_entry)
+def go_back():
+    if not history_key in st.session_state or len(st.session_state[history_key]) == 0:
+        st.toast('No more items to go back to!')
+        return
+    st.session_state[session_state_key] = st.session_state[history_key].pop()
+
+left.button("Go Back", use_container_width=True, on_click=go_back)
+middle.button("Skip", use_container_width=True, on_click=skip_entry)
 right.button("Save & Next", use_container_width=True, on_click=save_difficulty_load_new_item)
